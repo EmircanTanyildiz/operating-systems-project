47ffedf7ba842d72017a2422eb814a4475ddce72
Emir Can Tanyıldız <117983065+EmircanTanyildiz@users.noreply.github.com>
2025-04-04 21:01:13 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
-#include <iostream>
+#include "olay.h"
 
-using namespace std;
 int main() {
-    
+    Olay olay;
+    int zaman = 0;
+
+    olay.setOlaySayisi();   // Rastgele olay sayısı belirlenir
+    olay.setOlaySuresi();   // Her olayın süresi belirlenir (hepsi aynı)
+
+    int olaySayisi = olay.getOlaySayisi();
+    int sure = olay.getOlaySuresi();
+
+    for (int i = 0; i < olaySayisi; ++i) {
+        olay.olayUret(sure, &zaman); // Her olay zamanı güncelleyerek çıktı verir
+    }
+
     return 0;
 }
